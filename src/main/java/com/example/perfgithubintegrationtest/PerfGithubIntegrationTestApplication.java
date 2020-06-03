df5bf211c3ba8ed2d84a66d5787b7971e9a20986
Viktor Gritsaenko <vgritsaenko@epam.com>
2020-06-03 14:47:00 +0300

    Test pull request

--- src/main/java/com/example/perfgithubintegrationtest/PerfGithubIntegrationTestApplication.java
+++ src/main/java/com/example/perfgithubintegrationtest/PerfGithubIntegrationTestApplication.java
@@ -3,6 +3,7 @@ package com.example.perfgithubintegrationtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Comment
 @SpringBootApplication
 public class PerfGithubIntegrationTestApplication {
 
