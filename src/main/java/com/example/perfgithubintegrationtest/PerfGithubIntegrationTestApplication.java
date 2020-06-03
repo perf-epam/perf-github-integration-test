35a6104c95f37de16df2e59330a7a3cbdcac41d5
Viktor Gritsaenko <vgritsaenko@epam.com>
2020-06-03 14:46:18 +0300

    Initial commit

---
+++ src/main/java/com/example/perfgithubintegrationtest/PerfGithubIntegrationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.perfgithubintegrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfGithubIntegrationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfGithubIntegrationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/perfgithubintegrationtest/PerfGithubIntegrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perfgithubintegrationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfGithubIntegrationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
